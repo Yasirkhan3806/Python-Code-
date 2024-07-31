6ad9ddac37bdd0a68242ffd785fc457e068f8dac
Yasir Khan <yk5645123@gmail.com>
2024-07-30 22:46:01 -0700

    Day 1 & 2

---
+++ porject1.py
@@ -0,0 +1,47 @@
+# name = input("whats your name? ")
+# color = input("whats your fav color? ")
+# print(name + " Likes " + color)
+
+# birth_year = input("whats your age? ")
+# age = 2024 - int(birth_year)
+# fuck_you = "what the hell does that supposed to mean? "
+# print(age)
+# print(type(birth_year))
+
+# weight = input("whats your weight? ")
+# weight_in_kg = float(weight) * 0.453592
+# print(weight_in_kg)
+
+# String Techniques:
+# course = "Python for beginners"
+# print(course[0:3]) #this technique will return the string character inlcuding the first index defined but the excluded the character on second index and but default their value are [0:last character of the string]
+
+# Formatted Strings
+# First_name = 'Yasir'
+# Last_Name = 'Khan'
+#Now there are two ways to print this
+# message = First_name + ' [ ' + Last_Name + ' ] ' + 'is the best coder in the world'
+# print(message)
+# #Or we use formatted string that starts with f with is more simple and understandable
+# msg = f'{First_name} [{Last_Name}] is the best coder in the world'
+# print(msg)
+
+# STRING METHOD ,RUN THE FOLLOWING PROGRAM TO UNDERSTAND
+# Remeber all of the mention methods are case sensitive
+# message = "How are you"
+# print(len(message))
+# print(message.upper())
+# print(message.lower())
+# print(message.find('How'))
+# print(message.replace('are', 'my'))
+# # in operator is special boolean operator that finds a word in a string and return boolean value lets see(it is also case sensitive
+# print('you' in message)
+
+# Some tricky new arth operators
+# # float division
+# print(10 / 3)
+# #Integar division
+# print(10 // 3)
+# #Power operator
+# print(10 ** 3)
+
