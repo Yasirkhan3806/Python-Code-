7a5b058a375260db45a3bb68aca8aa4cc41c6c65
Yasir Khan <yk5645123@gmail.com>
2024-08-10 03:47:17 -0700

    started

---
+++ additional.py
@@ -0,0 +1,9 @@
+# here we will discuss some additional concepts like:
+# 1. SETS , a datatype that eliminates duplicates and save only distinct values
+# arr = [2,110,4,6,8,113,9,112,110,3,5,3]
+
+# set1 = set()
+# for i in arr:
+#     set1.add(i)
+
+# print(sorted(set1))
