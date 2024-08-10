c88ed1bae771c242f2bfe74a36e96f9861a2e1fa
Yasir Khan <yk5645123@gmail.com>
2024-08-10 09:34:01 -0700

    list comprehension

---
+++ list_comp.py
@@ -0,0 +1,7 @@
+# List comprehension is a technique that is used to write logic inside in an array to create an array lets see what this means 
+
+# words = ['this','means','you','are','born']
+
+# # words_uppercased = [word.upper() for word in words] List comprehension
+
+# print(*words_uppercased)
